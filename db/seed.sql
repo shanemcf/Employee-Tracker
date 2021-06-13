f3682c09ca76e7fcd14ae300e9b73f5485e3db91
shanemcf <shanemcfadden95@yahoo.com>
2021-06-13 11:55:55 -0500

    created values for roles

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,16 @@
+USE employee_tracker;
+
 INSERT INTO department (name)
 VALUES ("sales"),
         ("engineer"),
         ("accountant"),
         ("lawyer");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Sales", 50000, 1)        
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 80000, 2)        
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 60000, 3)        
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team", 100000, 4)        
